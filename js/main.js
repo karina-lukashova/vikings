b811f17eff04ca77c55670d538f74d089f921ef3
Karina Lukashova <karina-luka@yandex.ru>
2020-10-18 22:32:22 +0300

    Сделала разделы Все сезоны и Главные герои

--- js/main.js
+++ js/main.js
@@ -2,9 +2,25 @@ $(function(){
 
   $('[data-fancybox]').fancybox({
     youtube : {
-        controls : 0,
+        controls : 1,
         showinfo : 0
     }
+  });
+
+
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slider-arrow-prev.png" alt="Перейти к предыдущему герою"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slider-arrow-next.png" alt="Перейти к следующему герою"></button>'
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows: false
   });
 
 });
