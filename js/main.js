1f6689b3fb689ebdc4e967adf553ed56f0f17662
Karina Lukashova <karina-luka@yandex.ru>
2020-10-22 20:57:25 +0300

    Добавила адаптив

--- js/main.js
+++ js/main.js
@@ -7,14 +7,23 @@ $(function(){
     }
   });
 
-
   $('.heroes__slider-img').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-text',
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slider-arrow-prev.png" alt="Перейти к предыдущему герою"></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slider-arrow-next.png" alt="Перейти к следующему герою"></button>'
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slider-arrow-next.png" alt="Перейти к следующему герою"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
   });
+
   $('.heroes__slider-text').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -23,4 +32,13 @@ $(function(){
     arrows: false
   });
 
+});
+
+
+var menuBtn = document.querySelector('.menu__btn');
+var menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', function () {
+  menuList.classList.toggle('menu__list--active');
+  menuBtn.classList.toggle('menu__btn--active');
 });
